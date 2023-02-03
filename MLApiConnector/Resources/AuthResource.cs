c55845b876d40798d6281c36898708a582e083b0
Marco Cantu <Marco Cantu@DESKTOP-RRD415J>
2023-02-03 04:29:36 -0600

    Agregar archivos de proyecto.

---
+++ MLApiConnector/Resources/AuthResource.cs
@@ -0,0 +1,51 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MLApiConnector.Resources
+{
+    public class AuthResource
+    {
+        private RestClient _client;
+        private string baseResources = "https://api.mercadolibre.com";
+        private string resouces = "oauth/token";
+        private int timeOut = 5000;
+
+        public AuthResource()
+        {
+            _client = new RestClient();
+        }
+
+        public AuthResource(RestClientOptions clientOptions)
+        {
+            _client = new RestClient(clientOptions);
+        }
+
+        public AuthResource(RestClient client,string baseResources, string resouces, int timeOut=5000)
+        {
+            _client = new RestClient();
+            this.baseResources = baseResources;
+            this.resouces = resouces;
+            this.timeOut= timeOut;
+
+        }
+
+        public RestResponse GetToken(string clientId, string clientSecret, string code, string redirectUri) {
+            _client.Options.MaxTimeout = timeOut;
+            var uri= new Uri(baseResources+"/"+resouces);
+            var request = new RestRequest(uri,Method.Post);
+            request.AddHeader("Accept", "application/json");
+            request.AlwaysMultipartFormData = true;
+            request.AddParameter("grant_type", "authorization_code");
+            request.AddParameter("client_id", clientId);
+            request.AddParameter("client_secret", clientSecret);
+            request.AddParameter("code", code);
+            request.AddParameter("redirect_uri", "https://localhost:7192");
+            RestResponse response = _client.Execute(request);
+            return response;
+        }
+    }
+}
---
+++ UnitTesting/MLApiConnector/AuthResourceTests.cs
@@ -0,0 +1,40 @@
+﻿using MLApiConnector.Resources;
+using Moq;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using RichardSzalay.MockHttp;
+
+namespace UnitTesting.MLApiConnector
+{
+    public class AuthResourceTests
+    {
+        private MockHttpMessageHandler _httpMessageHandlerMock;
+        private AuthResource _authResource;
+
+        public AuthResourceTests()
+        {
+            
+            _httpMessageHandlerMock = new MockHttpMessageHandler();
+
+            _httpMessageHandlerMock
+            .When("https://api.mercadolibre.com/oauth/token")
+            .Respond("application/json", "{\"access_token\":\"APP_USR-6239883022283152-013022-2020b152d68d5812cfedd66b9ba9860d-43051780\",\"token_type\":\"Bearer\",\"expires_in\":21600,\"scope\":\"offline_access read write\",\"user_id\":43051780,\"refresh_token\":\"TG-63d8780226ceb3000192cf56-43051780\"}");
+
+            _authResource = new AuthResource(new RestClientOptions { ConfigureMessageHandler= _=> _httpMessageHandlerMock });
+        }
+
+        [Fact]
+        public void ShouldGetToken_Success()
+        {
+            var response = _authResource.GetToken("clientid", "clientSecret", "code", "localhost");
+
+            Assert.NotNull(response);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
